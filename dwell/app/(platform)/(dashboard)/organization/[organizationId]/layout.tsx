99e62117e1d320b9cb17121e94503a8ca168b7fc
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-12-19 08:08:50 +0000

    Create organization id layout

---
+++ dwell/app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,13 @@
+const OrganizationIdLayout = ({
+    children
+}:{
+    children: React.ReactNode;
+})=>{
+    return(
+        <>
+            {children}
+        </>
+    );
+};
+
+export default OrganizationIdLayout;
