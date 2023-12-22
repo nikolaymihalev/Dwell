e684ccf920b0d0219cd49486f180dabbfd8f1c9c
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-12-22 12:25:08 +0000

    Implement interface in navitem

--- dwell/app/(platform)/(dashboard)/_components/nav-item.tsx
+++ dwell/app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -1,6 +1,25 @@
 "use client";
 
-export const NavItem = ()=>{
+export type Organization = {
+    id:string;
+    slug: string;
+    imageUrl: string;
+    name: string;
+}
+
+interface NavItemProps {
+    isExpanded: boolean;
+    isActive: boolean;
+    organization: Organization;
+    onExpand: (id: string) => void;
+}
+
+export const NavItem = ({
+    isExpanded,
+    isActive,
+    organization,
+    onExpand,
+}: NavItemProps)=>{
     return (
         <div>
 
