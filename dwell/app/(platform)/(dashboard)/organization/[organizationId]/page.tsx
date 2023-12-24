5c0bb537045a6aa9bea8249655be2ea87e0abbf9
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-12-24 10:23:09 +0000

    Implement create board class

--- dwell/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ dwell/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,10 +1,12 @@
 "use client";
+import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
 import { OrganizationSwitcher } from "@clerk/nextjs";
 
 const OrganizationIdPage = () => {    
     return (
         <div>
-            <form >
+            <form action = {create}>
                 <input
                     id="title"
                     name = "title"
@@ -12,6 +14,9 @@ const OrganizationIdPage = () => {
                     className="border-black border p-1"
                     placeholder="Enter a board title"
                 />
+            <Button type = "submit">
+                Submit
+            </Button>
             </form>
         </div>
     );
