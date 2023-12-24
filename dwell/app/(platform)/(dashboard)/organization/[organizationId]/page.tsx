9c06add147378c694dde3623ef4ba90bcdf895b8
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-12-24 10:17:31 +0000

    Create db class

--- dwell/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ dwell/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,11 +1,21 @@
+"use client";
 import { OrganizationSwitcher, } from "@clerk/nextjs";
 
 const OrganizationIdPage = () => {
+    async function create(formData:FormData) {
+        "use server"
+    }
     return (
         <div>
-            <OrganizationSwitcher
-                hidePersonal
-            />
+            <form action={create}>
+                <input
+                    id="title"
+                    name = "title"
+                    required
+                    className="border-black border p-1"
+                    placeholder="Enter a board title"
+                />
+            </form>
         </div>
     );
 };
