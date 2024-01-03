a3c6ae1650352a39c4295cff9357022c00561fe5
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-03 07:07:02 +0000

    Update create-board.ts

--- dwell/actions/create-board.ts
+++ dwell/actions/create-board.ts
@@ -1,7 +1,14 @@
 import { db } from "@/lib/db";
+import { z } from "zod";
+
+const CreateBoard = z.object({
+    title: z.string(),
+});
 
 export async function create(formData:FormData) { 
-    const title = formData.get("title");
+    const {title} = CreateBoard.parse({
+        title: formData.get("title"),
+    });
 
     await db.board.create({
         data: {
