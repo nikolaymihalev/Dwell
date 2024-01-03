4b60d83bbf7124bfea9d7e0867ffdf419d71ec6e
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-03 07:21:07 +0000

    Create delete board function

---
+++ dwell/actions/delete-board.ts
@@ -0,0 +1,11 @@
+import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
+
+export async function deleteBoard(id:string) {
+    await db.board.delete({
+        where:{
+            id
+        }
+    });
+    revalidatePath("/organization");
+}
