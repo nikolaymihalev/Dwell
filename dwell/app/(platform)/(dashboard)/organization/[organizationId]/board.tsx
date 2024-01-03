9879d0eb7950a6778d0f28e2b95afbeee836d882
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-03 07:23:18 +0000

    Implement delete board action

--- dwell/app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
+++ dwell/app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
@@ -1,3 +1,4 @@
+import { deleteBoard } from "@/actions/delete-board";
 import { Button } from "@/components/ui/button";
 
 interface BoardProps {
@@ -9,8 +10,9 @@ export const Board = ({
     title,
     id
 }: BoardProps) => {
+    const deleteBoardWithId = deleteBoard.bind(null,id);
     return (
-        <form className="flex items-center gap-x-2">
+        <form action = {deleteBoardWithId} className="flex items-center gap-x-2">
             <p>
                 Board title: {title}
             </p>
