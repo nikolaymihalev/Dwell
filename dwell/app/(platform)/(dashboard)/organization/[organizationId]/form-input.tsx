607505b20e1c0f395d264002c98e81cfe98e85d8
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-04 07:51:36 +0000

    Create form input class

---
+++ dwell/app/(platform)/(dashboard)/organization/[organizationId]/form-input.tsx
@@ -0,0 +1,28 @@
+interface FormInputProps {
+    errors?: {
+        title?: string[];
+    }
+}
+
+export const FormInput = ({errors}:FormInputProps) => {
+    return (
+        <div>
+            <input
+                id="title"
+                name = "title"
+                required
+                className="border-black border p-1"
+                placeholder="Enter a board title"
+            />
+            {errors?.title ? (
+                <div>
+                    {errors.title.map((error: string)=> (
+                        <p key={error} className="text-rose-500">
+                            {error}
+                        </p>
+                    ))}
+                </div>
+            ):null}
+        </div>        
+    );
+};
