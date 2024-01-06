e6e841593df4b1c9687191ba3f4c3f79ba1c5b94
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-06 12:55:08 +0000

    Update form-input

--- dwell/components/form/form-input.tsx
+++ dwell/components/form/form-input.tsx
@@ -2,6 +2,9 @@
 
 import { forwardRef } from "react";
 import { useFormStatus } from "react-dom";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { cn } from "@/lib/utils";
 
 interface FormInputProps {
     id: string;
@@ -33,8 +36,15 @@ export const FormInput = forwardRef<HTMLInputElement, FormInputProps>(({
     return (
         <div className="space-y-2">
             <div className="space-y-1">
-
-            </div>
+                {label ? (
+                    <Label htmlFor={id} className = "text-xs font-semibold text-neutral-700">
+                        Label 
+                    </Label>
+                ):null}
+                <Input onBlur={onBlur} defaultValue={defaultValue} ref = {ref} required={required} name={id} id={id} 
+                    placeholder={placeholder} type={type} disabled={pending||disabled} className={cn( "text-sm px-2 py-1 h-7", className,)} aria-describedby={`${id}-error`}/>
+            </div>  
+            <FormErrors id={id} errors={errors}/>
         </div>
     )
 });
