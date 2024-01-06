746c6b4a98ff77895a89349d919beb1b780f7b01
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-06 13:03:44 +0000

    Update form with form input

--- dwell/app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ dwell/app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -3,9 +3,9 @@
 import { createBoard } from "@/actions/create-board";
 import { Button } from "@/components/ui/button";
 import { useFormState } from "react-dom";
-import { FormInput } from "./form-input";
 import { FormButton } from "./form-button";
 import { useAction } from "@/hooks/use-action";
+import { FormInput } from "@/components/form/form-input";
 
 export const Form=()=>{
     const {execute, fieldErrors} = useAction(createBoard,{
@@ -24,7 +24,7 @@ export const Form=()=>{
     return (
         <form action = {onSubmit}>
             <div className="flex flex-col space-y-2">            
-                <FormInput errors={fieldErrors}/>                
+                <FormInput id="title" errors={fieldErrors}/>                
             </div>
             <FormButton/>
         </form>
