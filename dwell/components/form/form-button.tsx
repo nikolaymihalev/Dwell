2655dac9f99d5d370e8d4ab7b2d693f487cc1b7a
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-06 13:10:39 +0000

    Update form-button.tsx

--- dwell/components/form/form-button.tsx
+++ dwell/components/form/form-button.tsx
@@ -10,3 +10,18 @@ interface FormSubmitProps {
     className?: string;
     variant?: "default"|"destructive"|"outline"|"secondary"| "ghost"| "link"|"primary";
 };
+
+export const FormSubmit = ({
+    children,
+    disabled,
+    className,
+    variant
+}:FormSubmitProps)=>{
+    const {pending} = useFormStatus();
+
+    return (
+        <Button disabled={pending||disabled} type="submit" variant={variant} size="sm" className={cn(className)}>
+            {children}
+        </Button>
+    );
+};
