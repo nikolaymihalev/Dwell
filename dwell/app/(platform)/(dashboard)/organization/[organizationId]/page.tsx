2f3de8ad8aae025ff25bf9ee6e6eea5aca603357
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-16 07:26:06 +0000

    Implement info class

--- dwell/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ dwell/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,8 +1,10 @@
+import { Info } from "./_components/info";
+
 const OrganizationIdPage = async () => {    
 
     return (
         <div className="w-full mb-20">
-            
+            <Info/>            
         </div>
     );
 };
