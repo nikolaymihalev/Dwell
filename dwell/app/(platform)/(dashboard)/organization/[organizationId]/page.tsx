f9b1fe98fc9b94e498e642b3cf196c7d24517dc9
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-16 07:36:37 +0000

    Implement board list

--- dwell/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ dwell/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,10 +1,14 @@
+import { BoardList } from "./_components/board-list";
 import { Info } from "./_components/info";
 
 const OrganizationIdPage = async () => {    
 
     return (
         <div className="w-full mb-20">
-            <Info/>            
+            <Info/>     
+            <div className="px-2 md:px-4">
+                <BoardList/>
+            </div>       
         </div>
     );
 };
