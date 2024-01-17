4fd4e0f04f20a135315bc782c4bd036b5c7ebf8e
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-17 08:31:03 +0000

    Create form popover class

---
+++ dwell/components/form/form-popover.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
+import { useAction } from "@/hooks/use-action";
+import { createBoard } from "@/actions/create-board";
+import { FormInput } from "./form-input";
+import { FormSubmit } from "./form-submit";
+
+interface FormPopoverProps {
+    children: React.ReactNode;
+    side?: "left"|"right"|"top"|"bottom";
+    align?:"start"|"center"|"end";
+    sideOffset?:number;
+};
+
+export const FormPopover = ({
+    children,
+    side = "bottom",
+    align,
+    sideOffset = 0,
+}: FormPopoverProps)=>{
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                {children}
+            </PopoverTrigger>
+            <PopoverContent align={align} className="w-80 pt-3" side={side} sideOffset={sideOffset}>
+                <div className="text-sm font-medium text-center text-neutral-600 pb-4">
+                    Create board
+                </div>
+            </PopoverContent>
+        </Popover>
+    );
+};
