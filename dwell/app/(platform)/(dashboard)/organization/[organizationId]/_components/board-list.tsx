21ac186596ed2d5c765537ae0fda289a4342313e
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-17 08:33:38 +0000

    Implement popover in board list

--- dwell/app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ dwell/app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,3 +1,4 @@
+import { FormPopover } from "@/components/form/form-popover";
 import { Hint } from "@/components/hint";
 import { HelpCircle, User2 } from "lucide-react";
 
@@ -8,16 +9,19 @@ export const BoardList = () => {
                 <User2 className="h-6 w-6 mr-2"/>
                 Your boards
             </div>
-            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
-                <div role="button" className="aspect-video relative h-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition">
-                    <p className="text-sm">Create new board</p>
-                    <span className="text-xs">
-                        5 remaining
-                    </span>
-                    <Hint sideOffset={40} desription={`Free workspaces can have up to 5 boards`}>
-                        <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]"/>
-                    </Hint>
-                </div>
+            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+                <FormPopover sideOffset={10} side="right">
+                    <div role="button" className="aspect-video relative h-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition">
+                        <p className="text-sm">Create new board</p>
+                        <span className="text-xs">
+                            5 remaining
+                        </span>
+                        <Hint sideOffset={40} desription={`Free workspaces can have up to 5 boards`}>
+                            <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]"/>
+                        </Hint>
+                    </div>
+                </FormPopover>
+                
             </div>
         </div>
     );
