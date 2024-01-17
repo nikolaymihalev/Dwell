3e85ea6f294aa5b3e465525e3198c98b915f561b
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-17 07:45:13 +0000

    Update hint.tsx

--- dwell/components/hint.tsx
+++ dwell/components/hint.tsx
@@ -1,1 +1,28 @@
-import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip"
+import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip";
+
+interface HintProps {
+    children: React.ReactNode;
+    desription: string;
+    side?: "left"|"right"|"top"|"bottom";
+    sideOffset?: number;
+};
+
+export const Hint = ({
+    children,
+    desription,
+    side = "bottom",
+    sideOffset = 0
+}:HintProps)=>{
+    return (
+        <TooltipProvider>
+            <Tooltip delayDuration={0}>
+                <TooltipTrigger>
+                    {children}
+                </TooltipTrigger>
+                <TooltipContent sideOffset={sideOffset} side = {side} className="text-xs max-w-[220px] break-words">
+                    {desription}
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+    );
+};
