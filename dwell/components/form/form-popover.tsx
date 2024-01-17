2aadb63474f7ec2e7f24a055c788de61c23ba993
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-17 08:39:28 +0000

    Add close button and form in list

--- dwell/components/form/form-popover.tsx
+++ dwell/components/form/form-popover.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
+import {Popover, PopoverClose, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
 import { useAction } from "@/hooks/use-action";
 import { createBoard } from "@/actions/create-board";
 import { FormInput } from "./form-input";
 import { FormSubmit } from "./form-submit";
+import { Button } from "../ui/button";
+import { X } from "lucide-react";
 
 interface FormPopoverProps {
     children: React.ReactNode;
@@ -28,6 +30,16 @@ export const FormPopover = ({
                 <div className="text-sm font-medium text-center text-neutral-600 pb-4">
                     Create board
                 </div>
+                <PopoverClose asChild>
+                    <Button className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600" variant="ghost">
+                          <X className="h-4 w-4"/>  
+                    </Button>
+                </PopoverClose>
+                <form>
+                    <div className="space-y-4">
+                        <FormInput id="title" label="Board title" type="text"/>
+                    </div>
+                </form>
             </PopoverContent>
         </Popover>
     );
