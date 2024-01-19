2de04cc2f9704817af0652b88ba1246c46bc8395
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-19 07:40:01 +0000

    Implement toaster to platform layout

--- dwell/components/form/form-popover.tsx
+++ dwell/components/form/form-popover.tsx
@@ -52,7 +52,7 @@ export const FormPopover = ({
                 </PopoverClose>
                 <form action={onSubmit} className="space-y-4">
                     <div className="space-y-4">
-                        <FormInput id="title" label="Board title" type="text"/>
+                        <FormInput id="title" label="Board title" type="text" errors = {fieldErrors}/>
                     </div>
                     <FormSubmit className="w-full">
                         Create
